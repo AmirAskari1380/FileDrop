cd0effac73c78678adacfb2b0006bc2d6ae5afb3
AmirAskari1380 <91520778+AmirAskari1380@users.noreply.github.com>
2022-09-09 13:18:46 +0430

    Initial work
    Adding the initial styling and functionality of the project

---
+++ script.js
@@ -0,0 +1,22 @@
+const label = document.querySelector("label");
+
+function onEnter() {
+    label.classList.add("active");
+}
+
+function onLeave() {
+    label.classList.remove("active")
+}
+
+label.addEventListener("dragenter", onEnter);
+label.addEventListener("drop", onLeave);
+label.addEventListener("dragend", onLeave);
+label.addEventListener("dragleave", onLeave);
+label.addEventListener("dragexit", onLeave);
+
+const input = document.querySelector("input");
+input.addEventListener("change", event =>{
+    if(input.files.length > 0) {
+        console.log("File Added!")
+    }
+})
