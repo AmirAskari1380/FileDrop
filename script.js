db9fc76fa39f9f005a31145f4c90092d693d2b39
Amir <91520778+AmirAskari1380@users.noreply.github.com>
2022-10-12 12:28:49 +0330

    Fixing whitespaces
    Fixing whitespaces, such as  spaces and tabs

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const label = document.querySelector("label");
 
 function onEnter() {
-    label.classList.add("active");
+  label.classList.add("active");
 }
 
 function onLeave() {
-    label.classList.remove("active")
+  label.classList.remove("active");
 }
 
 label.addEventListener("dragenter", onEnter);
@@ -15,8 +15,8 @@ label.addEventListener("dragleave", onLeave);
 label.addEventListener("dragexit", onLeave);
 
 const input = document.querySelector("input");
-input.addEventListener("change", event =>{
-    if(input.files.length > 0) {
-        console.log("File Added!")
-    }
-})
+input.addEventListener("change", event => {
+  if (input.files.length > 0) {
+    console.log("File Added!");
+  }
+});
